805ff8175f200e78c9142c58d28790f1d0cf6ae1
Mohamed Khalifa <mohamed.khalifa01@eng-st.cu.edu.eg>
2023-06-14 14:59:38 -0400

    adv #100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,6 @@
 def weight_average(my_list=[]):
     num, den = 0, 0
     for li in my_list:
-        num += li[0]* li[1]
-        den += li[1] 
+        num += li[0] * li[1]
+        den += li[1]
     return num/den
