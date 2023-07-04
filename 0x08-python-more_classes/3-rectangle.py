60205777d7ecef4dbdd6c2540897982a4f540876
Mohamed Khalifa <mohamed.khalifa01@eng-st.cu.edu.eg>
2023-07-04 09:45:33 -0400

    Task #3

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""3 - rectangle with width and hight"""
+
+
+class Rectangle:
+    """define width and hight"""
+
+    def __init__(self, width=0, height=0):
+        """instantiation of the class"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """get the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set the width with error detection"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """"get the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set the height with error detection"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2*(self.__width + self.__height)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rec_str = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                rec_str += '#'
+            if i != self.__height - 1:
+                rec_str += '\n'
+        return rec_str
